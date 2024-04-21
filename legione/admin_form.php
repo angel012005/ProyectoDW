40fde73f63ebe61a5f45ca072d8869c0a21b2d17
eze <ezequieldurante640@gmail.com>
2024-04-21 20:24:38 -0300

    esta el enviar img

--- legione/admin_form.php
+++ legione/admin_form.php
@@ -2,3 +2,7 @@
 
 $conex = new mysqli('localhost','root','mysqlezequiel1_','img');
 
+$nombre = $_POST['nombre'];
+$imagen = addslashes(file_get_contents($imagen));
+$consulta = "INSERT into images VALUES ('','$imagen')";
+$enviar= $conex->query($consulta);
