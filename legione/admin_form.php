5b5ddd526db5896e0b236a82d6916176d4023e8c
angel012005 <angelramirez0112@gmail.com>
2024-05-17 21:59:06 -0300

    se cambio admin php

--- legione/admin_form.php
+++ legione/admin_form.php
@@ -1,3 +1,5 @@
+
+
 <?php
 $conex = new mysqli('localhost','root','','img');
 $nombre = $_POST['nombre'];
@@ -5,6 +7,13 @@ $image = $_FILES["imagen"]["tmp_name"];
 $imgContent = addslashes(file_get_contents($image));
 $consulta = "INSERT into images VALUES (NULL,'$imgContent','$nombre')";
 $enviar= $conex->query($consulta);
+
+echo '
+<script>
+alert("Imagen subida correctamente");
+window.location = "Admin.php";
+</script>
+   ';
    
 
 
